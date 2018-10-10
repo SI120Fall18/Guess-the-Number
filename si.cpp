21df1a1da97a087003d6500b77523ebd408d1210
Allen-Sarmiento <sarmientoallenrey@gmail.com>
2018-10-10 15:52:30 -0700

    first game complete

--- si.cpp
+++ si.cpp
@@ -13,35 +13,106 @@ int main()
 
 	int max = 100;
 	int min = 1;
-	//int number = rand() % (max - (min + 1)) + min;
-	int number = 10;
-    int guess;
+	sleep(3);
+    int number = rand() % (max - (min + 1)) + min;
+	int guess;
 
-	std::cout << "Guess the number 1-100: ";
+	std::cout << "Guess the number 1-100. You have 3 tries or you will die: ";
 	std::cin >> guess;
-	sleep(5);
+	sleep(1);
 
 	if(guess == number){
 		
 		std::cout << "Correct!" << std::endl;
 
 	} else if(guess > number){
-	    
+	
 		std::cout << "Too High!" << std::endl;
+		
+		if (guess > 100) {
+			std::cout << "Not in range" << std::endl;
+		}
 
-        if(guess > 100) {
-            std::cout << "Not in range" << std::endl;
-        }
+
 	} else if(guess < number) {
 	
 		std::cout << "Too Low!" << std::endl;
-        
-        if(guess < 1) {
-            std::cout << "Not in range" << std::endl;
-        }
-	}
+
+		if (guess < 1) {
+			std::cout << "Not in range" << std::endl;
+}
+
+	} 
+	
+if(guess != number) {
+
+	std::cout << "Try again!" << std::endl;
+	std::cout << "Guess the number: ";
+	std::cin >> guess;
+	
+	if(guess == number){
+		
+		std::cout << "Correct!" << std::endl;
+
+	} else if(guess > number){
+	
+		std::cout << "Too High!" << std::endl;
+		
+		if (guess > 100) {
+			std::cout << "Not in range" << std::endl;
+		}
 
 
+	} else if(guess < number) {
+
+	std::cout << "Too Low!" << std::endl;
+
+		if (guess < 1) {
+			std::cout << "Not in range" << std::endl;
+}
+
+	} 
+ }
+
+if(guess != number) {
+
+	std::cout << "Try again!" << std::endl;
+	std::cout << "Guess the number: ";
+	std::cin >> guess;
+	
+	if(guess == number){
+		
+		std::cout << "Correct!" << std::endl;
+
+	} else if(guess > number){
+	
+		std::cout << "Too High!" << std::endl;
+		
+		if (guess > 100) {
+			std::cout << "Not in range" << std::endl;
+		}
+
+
+	} else if(guess < number) {
+
+		std::cout << "Too Low!" << std::endl;
+
+		if (guess < 1) {
+			std::cout << "Not in range" << std::endl;
+}
+
+	} 
+ }
+
+if(guess != number) {
+	std::cout << "You died" << std::endl;
+}
+
+	std::cout << "The number is " << number << std::endl;
+
+
+	
+	
 	
 return 0;
 
